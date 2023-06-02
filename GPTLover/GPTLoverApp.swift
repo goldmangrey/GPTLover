d81d5caccd4884c6338e48348491c7660c43f1bb
Yeskendir <yeskendiriskakov@mail.ru>
2023-06-02 23:43:48 +0600

    Initial Commit

---
+++ GPTLover/GPTLoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  GPTLoverApp.swift
+//  GPTLover
+//
+//  Created by Yeskendir on 02.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct GPTLoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
